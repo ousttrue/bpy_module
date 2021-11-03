fb9827477d5db488054968388dbca437dd533b97
ousttrue <ousttrue@gmail.com>
2021-11-04 01:24:58 +0900

    bpy build

--- dodo.py
+++ dodo.py
@@ -10,8 +10,13 @@ REPO = Repo(CLONE_DIR)
 from doit.action import CmdAction
 
 CONFIGURE_FLAGS = ' '.join([
-    '-DWITH_INPUT_NDOF=OFF', '-DWITH_CYCLES=OFF', '-DWITH_OPENVDB=OFF',
-    '-DWITH_LIBMV=OFF'
+    '-DCMAKE_BUILD_TYPE=Release',
+    '-DWITH_INTERNATIONAL=OFF',
+    '-DWITH_INPUT_NDOF=OFF',
+    '-DWITH_CYCLES=OFF',
+    '-DWITH_OPENVDB=OFF',
+    '-DWITH_LIBMV=OFF',
+    '-DWITH_MEM_JEMALLOC=OFF',
 ])
 
 BPY_FLAGS = ' '.join([
@@ -20,22 +25,45 @@ BPY_FLAGS = ' '.join([
 ])
 
 
+def task__worktree():
+    if not REPO:
+        return
+    for tag in REPO.tags:
+        base_dir = HERE / f'tags/{tag.name}'
+        worktree = base_dir / 'blender'
+        yield {
+            'name':
+            tag.name,
+            'actions': [
+                CmdAction(f'git worktree add {worktree} {tag.name}',
+                          cwd=CLONE_DIR),
+                CmdAction(f'git submodule update --init', cwd=worktree)
+            ],
+            'uptodate': [True],
+            'targets':
+            [worktree / 'release/scripts/addons/io_scene_obj/__init__.py']
+        }
+
+
 def task_bpy_build():
     if not REPO:
         return
     for tag in REPO.tags:
         base_dir = HERE / f'tags/{tag.name}'
-        workspace = base_dir / 'blender'
-        build = base_dir / 'bpy'
+        install = base_dir / 'bpy_install'
         yield {
             'name':
             tag.name,
+            'task_dep': [f'_worktree:{tag.name}'],
             'verbosity':
             2,
             'actions': [
                 CmdAction(
                     f'cmake -S blender -B bpy -G Ninja {CONFIGURE_FLAGS} {BPY_FLAGS}',
                     cwd=base_dir),
-                CmdAction(f'cmake --build bpy', cwd=base_dir)
+                CmdAction(f'cmake --build bpy', cwd=base_dir),
+                CmdAction(
+                    f'cmake --install bpy --config Release --prefix {install}',
+                    cwd=base_dir)
             ],
         }
